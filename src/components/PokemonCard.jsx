1712a9fcb4c753539351fe2262646fa348b6e527
Baron Nicolas <baronnico@hotmail.fr>
2023-03-28 15:51:39 +0200

    modif pokedex

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,20 @@
 function PokemonCard() {
-    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /></figure>;
+  let pokemon = pokemonList[1];
+  let condition = pokemon.imgSrc ? <img src={pokemon.imgSrc} alt="Bulbasaur"/> : <p>???</p>;
+   
+    return <figure>{condition}</figure>;
     <figcaption>bulbasaur</figcaption>
-}
+
+    }
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 export default PokemonCard;
