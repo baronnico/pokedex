754d3ca4dfdb9a78a38cee92fcf763af59b864b6
Baron Nicolas <baronnico@hotmail.fr>
2023-03-28 11:34:27 +0200

    bulbasaur creation

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,5 @@
+function PokemonCard() {
+    return <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" /></figure>;
+    <figcaption>bulbasaur</figcaption>
+}
+export default PokemonCard;
